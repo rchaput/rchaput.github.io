480e19b3929028d8b099cee55bb40e4572a09b28
rchaput <rchaput.pro@gmail.com>
2020-11-29 20:12:23 +0100

    Removed useless files and updated the `view` script to bind to 0.0.0.0 instead of localhost

--- view.sh
+++ view.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-hugo server --disableFastRender --i18n-warnings
+# TODO: find the remote IP (e.g. with `hostname -I`) and bind to it
+# instead of allowing any connection
+hugo server --disableFastRender --i18n-warnings --bind=0.0.0.0
